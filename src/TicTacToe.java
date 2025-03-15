ef4b76162eaf2b19728253d03f946995dc78fd34
Vinay Varma <vinayvarma5208@gmail.com>
2025-03-15 14:20:16 -0400

    updated code so that it can switch between user and computer and store values in game board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -13,42 +14,18 @@ public class TicTacToe {
         printGameBoard(gameBoard);
 
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number in between (1 to 9): ");
-        int position = scanner.nextInt();
-        System.out.print(position);
 
-        switch (position) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-            case 3:
-                gameBoard[0][4] = 'X';
-                break;
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-            case 6:
-                gameBoard[2][4] = 'X';
-                break;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter number in between (1 to 9): ");
+            int playerPosition = scanner.nextInt();
+            placePiece(gameBoard, playerPosition,"player");
 
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-            case 9:
-                gameBoard[4][4] = 'X';
-                break;
+            Random random = new Random();
+            int computerPosition = random.nextInt(9)+ 1;
+            placePiece(gameBoard, computerPosition,"computer");
+            printGameBoard(gameBoard);
         }
-        printGameBoard(gameBoard);
 
     }
     public static  void printGameBoard(char[][] gameBoard) {
@@ -57,6 +34,48 @@ public class TicTacToe {
                 System.out.print(col);
             }
             System.out.println();
+        }
+    }
+
+    public static void placePiece(char[][] gameBoard, int position, String user) {
+        char symbol = ' ';
+        if (user.equals("player")) {
+            symbol = 'X';
+        }
+        else if (user.equals("computer")) {
+            symbol = 'O';
+        }
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = symbol;
+                break;
+            case 2:
+                gameBoard[0][2] = symbol;
+                break;
+            case 3:
+                gameBoard[0][4] = symbol;
+                break;
+            case 4:
+                gameBoard[2][0] = symbol;
+                break;
+            case 5:
+                gameBoard[2][2] = symbol;
+                break;
+            case 6:
+                gameBoard[2][4] = symbol;
+                break;
+
+            case 7:
+                gameBoard[4][0] = symbol;
+                break;
+            case 8:
+                gameBoard[4][2] = symbol;
+                break;
+            case 9:
+                gameBoard[4][4] = symbol;
+                break;
+            default:
+                break;
         }
     }
 }
